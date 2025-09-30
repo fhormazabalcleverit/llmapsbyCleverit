257410ab059de142769562811afd985839da582d
fhormazabalcleverit <fhormazabal@cleveritgroup.com>
2025-09-30 11:18:13 -0300

    Updated Process.tsx

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -81,7 +81,7 @@ const Process = () => {
                 {step.title}
               </h3>
               <div className="flex-1 flex items-end">
-                <p className="text-gray-400 leading-relaxed opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-500 delay-200">
+                <p className="text-gray-400 leading-relaxed text-boldopacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-500 delay-200">
                   {step.description}
                 </p>
               </div>

8ad590bb4b52cab1af971788e9d177ca80c4462d
Francisco - Product Designer <fhormazabal@cleveritgroup.com>
2025-10-01 16:44:12 -0300

    Updated TeamPage.tsx

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -11,7 +11,7 @@ const TeamPage = () => {
       <div className="absolute inset-0 bg-no-repeat bg-center bg-[length:150%_auto] md:bg-cover"
         style={{ backgroundImage: 'url(/weare.svg)' }}></div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-24 relative z-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-24 relative z-10">
         <div className="grid grid-cols-1 lg:grid-cols-5 gap-12 lg:gap-16 items-center mb-20">
           <div className="space-y-8 col-span-3">
 

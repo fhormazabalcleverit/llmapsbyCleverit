0e9eb9810c6e3759297873f71147be9212c0b79a
Francisco - Product Designer <fhormazabal@cleveritgroup.com>
2025-10-01 14:50:44 -0300

    Updated TeamPage.tsx

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -57,7 +57,7 @@ const TeamPage = () => {
 
 
       
-      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-24">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-24">
         <h2 className="text-3xl md:text-4xl font-bold text-white text-center mb-16">
           {t.team.skills.title}
         </h2>
